a08d20d3cd81f93936a4a7c5c119ca07bf13db58
Will <will@esotericcoder.com>
2018-06-25 01:33:53 -0700

    Level 3

--- MainActivity.java
+++ MainActivity.java
@@ -5,28 +5,30 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements View.OnLongClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnLongClickListener, MyParentFragment.Listener {
+
+    public static final String TAG_MAIN_FRAGMENT = "MainFragment";
 
     private TaskViewModel mTaskViewModel;
     private TasksAdapter tasksAdapter;
-    private RecyclerView recyclerView;
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        recyclerView = findViewById(R.id.recyclerview);
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
         tasksAdapter = new TasksAdapter(new ArrayList<Task>(), this);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(tasksAdapter);
@@ -40,13 +42,28 @@ public class MainActivity extends AppCompatActivity implements View.OnLongClickL
             }
         });
 
-        FloatingActionButton fab = findViewById(R.id.fab);
+        fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Date currentTime = new Date();
-                Task task = new Task("Hello World", 1, currentTime);
-                mTaskViewModel.insert(task);
+                MyParentFragment fragment = new MyParentFragment();
+                fragment.setListener(MainActivity.this);
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.container, fragment, TAG_MAIN_FRAGMENT);
+                transaction.commit();
+                fab.hide();
+            }
+        });
+
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                if (dy > 0 && fab.getVisibility() == View.VISIBLE) {
+                    fab.hide();
+                } else if (dy < 0 && fab.getVisibility() != View.VISIBLE) {
+                    fab.show();
+                }
             }
         });
 
@@ -59,6 +76,15 @@ public class MainActivity extends AppCompatActivity implements View.OnLongClickL
         return true;
     }
 
+    @Override
+    public void onTaskReceived(Task task) {
+        mTaskViewModel.insert(task);
+        Fragment fragment = getSupportFragmentManager().findFragmentByTag(TAG_MAIN_FRAGMENT);
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction().remove(fragment).commit();
+            fab.show();
+        }
+    }
 }
 
 
